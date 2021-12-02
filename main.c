cadf1740221a4d37115255e6780fee5d5efbd329
Aysegul50 <92298095+Aysegul50@users.noreply.github.com>
2021-12-02 09:43:59 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+    int count=10, x;
+    int *int_pointer;
+
+    int_pointer=&count;
+    x=*int_pointer;
+    printf("count=%d, x=%i\n",count,x);
+    */
+
+    /*
+    char c = 'Q';
+    char *char_pointer= &c;
+
+    printf("%c %c\n",c,*char_pointer);
+
+    c='/';
+    printf("%c %c\n",c,*char_pointer);
+
+    *char_pointer='(';
+    printf("%c %c\n",c,*char_pointer);
+    */
+
+    /*verilen tek boyutlu bir dizinin elemanlarını tersten yazdıran kod
+    int a[10]={1,2,3,4,5,6,7,8,9,10};
+    int i;
+
+    for(int i=9; i >= 0; i--){
+
+      printf("%d\n",a[i]);
+    } */
+
+    /*
+    int x= 1 ,y = 2, *ip;;
+    ip = &x;
+
+
+    printf("x'in adresi = %p | y'nin adresi=%p\n\n",&x,&y);
+    printf("x= %d  | y=%d    | ip=&p\n",x,y,ip);
+
+    y=*ip;
+
+    printf("********************************************************\n");
+    printf("x'in adresi = %p | y'nin adresi=%p\n\n",&x,&y);
+    printf("x= %d  | y=%d   | ip=&p\n",x,y,ip);
+
+    x=ip;
+
+    printf("********************************************************\n");
+    printf("x'in adresi = %p | y'nin adresi=%p\n\n",&x,&y);
+    printf("x= %d | y=%d     | ip=&p\n",x,y,ip);
+
+    *ip=3;
+
+    printf("********************************************************\n");
+    printf("x'in adresi = %p | y'nin adresi=%p\n\n",&x,&y);
+    printf("x= %d | y=%d     | ip=&p\n",x,y,ip);
+
+    return 0;
+}*/
